80c4029bb50f6e08a25bc894274bbd37750bc275
jidekipro6 <jidekipro6@gmail.com>
2023-06-19 16:28:34 -0500

    intento de edspliege en render quijua

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-	private static final String template = "Hello, %s!";
+	private static final String template = "hola, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
-	@GetMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+	@GetMapping("/saludo")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "idat") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 }
